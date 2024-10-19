e6e34c713a3001181efbf4e680c673f271051a8a
Petar Stankovic <stankovicpetar23@gmail.com>
2024-10-19 19:44:51 +0200

    Deploy yaml bug fix. #5

---
+++ .github/workflows/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Navigiraj do foldera aplikacije
+cd /home/ubuntu/cocktail-app-backend
+
+# Povuci najnoviji kod sa GitHub-a
+echo "Pulling latest code from GitHub..."
+git reset --hard
+git pull origin master
+
+# Builduj i restartuj Docker kontejnere
+echo "Building Docker containers..."
+docker-compose -f docker-compose.prod.yml up --build -d
+
+# Opcionalno: Čišćenje starih Docker slika (ako koristiš image verzije)
+docker image prune -f
+
+echo "Deployment completed!"
