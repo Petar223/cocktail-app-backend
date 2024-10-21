beabc8556b3f0f1e04e1d92849c934a3d41c1f28
Petar Stankovic <stankovicpetar23@gmail.com>
2024-10-21 14:04:13 +0200

    Deploy yaml bug fix. #13

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -8,12 +8,16 @@ echo "Pulling latest code from GitHub..."
 git reset --hard
 git pull origin master
 
+docker network create cocktail-app-network
+
 # Builduj i restartuj Docker kontejnere
 echo "Building Docker containers..."
 docker-compose -f docker-compose.prod.yaml up --build -d
+
+docker run -ti --rm --net cocktail-app-network cocktail-app-backend-backend sh -c 'node importData.js'
 
 # Opcionalno: Čišćenje starih Docker slika (ako koristiš image verzije)
 docker image prune -f
 
 echo "Deployment completed!"
-echo "PORT: " $PORT
+
